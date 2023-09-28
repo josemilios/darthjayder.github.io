06dd98c17088ca8ca46781f659ee334064367b63
darthjayder <146266420+darthjayder@users.noreply.github.com>
2023-09-28 14:40:58 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
         var element = $(this).parents("div.container");
         var pos = event.target.value;
 
-        $("div.before").css({width: pos + "%"});
-        $("div.slider-button").css({left: "calc(" + pos + "% - 18px)"});
         element.find("div.before").css({width: pos + "%"});
         element.find("div.slider-button").css({left: "calc(" + pos + "% - 18px)"});
     });
